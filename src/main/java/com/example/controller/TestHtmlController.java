db54d6ac5418f680cbb5e55b08cd9b5debfb94fd
xiongtao <bigapple0829@163.com>
2017-08-25 09:57:48 +0800

    sss

--- src/main/java/com/example/controller/TestHtmlController.java
+++ src/main/java/com/example/controller/TestHtmlController.java
@@ -3,13 +3,6 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * @author xiongtao
- * @version v1.0
- * @classDesc: 功能描述:()
- * @cratedate 2017/8/25 9:40
- * @copyright:善林(上海)金融信息服务有限公司
- */
 
 @RequestMapping("/")
 @Controller
